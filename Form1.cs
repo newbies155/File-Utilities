386949d2ad26380b994ac921b765e55cb3023725
newbie155 <69512353+newbies155@users.noreply.github.com>
2021-07-18 16:30:35 +0300

    Changed namespace to not trigger people
    yeah its a useless change but it works

--- Form1.cs
+++ Form1.cs
@@ -14,7 +14,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WinFormsApp1
+namespace FileUtilities
 {
     public partial class Form1 : Form
     {
